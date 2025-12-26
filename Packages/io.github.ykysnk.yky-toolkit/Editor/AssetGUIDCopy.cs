b0f30733b47a6c5d9858b4bc0daeabe4ad1e156f
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-12-26 20:33:38 +0800

    refactor: Simplify MenuItem attribute in AssetGUIDCopy script

--- Packages/io.github.ykysnk.yky-toolkit/Editor/AssetGUIDCopy.cs
+++ Packages/io.github.ykysnk.yky-toolkit/Editor/AssetGUIDCopy.cs
@@ -7,9 +7,7 @@ namespace io.github.ykysnk.ykyToolkit.Editor;
 
 internal static class AssetGuidCopy
 {
-    private const string MenuPath = $"Assets/{Util.Name}/Asset GUID Copy";
-
-    [MenuItem(MenuPath, false)]
+    [MenuItem($"Assets/{Util.Name}/Asset GUID Copy")]
     private static void GuidCopy()
     {
         if (Selection.assetGUIDs.Length < 1) return;
