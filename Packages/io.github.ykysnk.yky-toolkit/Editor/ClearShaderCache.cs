f1b7cbbcd44157ace9e4fe27fbb7ad1219cea8dd
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-12-27 03:28:01 +0800

    refactor: Reintroduce utils dependency in ClearShaderCache and improve shader cache path handling

--- Packages/io.github.ykysnk.yky-toolkit/Editor/ClearShaderCache.cs
+++ Packages/io.github.ykysnk.yky-toolkit/Editor/ClearShaderCache.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using io.github.ykysnk.utils;
 using UnityEditor;
 using UnityEngine;
 
@@ -11,7 +12,9 @@ internal static class ClearShaderCache
     {
         if (!EditorUtility.DisplayDialog("Clear Shader Cache", "Are you sure you want to clear the Shader Cache?", "Yes",
                 "No")) return;
-        var shaderCachePath = Path.Combine(Application.dataPath, "../Library/ShaderCache");
+        var shaderCachePath =
+            Path.Combine(Directory.GetParent(Application.dataPath)?.FullName ?? "", "Library/ShaderCache");
+        Utils.Log(nameof(ClearShaderCache), $"Shader Cache Path: {shaderCachePath}");
         if (Directory.Exists(shaderCachePath)) Directory.Delete(shaderCachePath, true);
         EditorUtility.DisplayDialog("Shader Cache Cleared", "Shader Cache has been cleared.", "OK");
         AssetDatabase.Refresh();
