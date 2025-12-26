d54f26c5584aaa71495b00713a4e46d97fa2fa32
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-12-26 19:05:57 +0800

    feat: Add EmptyFolderClear utility to YKY Toolkit

---
+++ Packages/io.github.ykysnk.yky-toolkit/Editor/EmptyFolderClear.cs
@@ -0,0 +1,29 @@
+using System.IO;
+using System.Linq;
+using UnityEditor;
+
+namespace io.github.ykysnk.ykyToolkit.Editor;
+
+internal static class EmptyFolderClear
+{
+    private const string Title = "Clear Empty Folder";
+
+    [MenuItem($"Tools/{Util.Name}/{Title}")]
+    private static void Clear()
+    {
+        var reportPaths = Directory.GetDirectories("Assets/", "*", SearchOption.AllDirectories).Where(path =>
+            Directory.GetFiles(path).Length < 1 && Directory.GetDirectories(path).Length < 1).ToList();
+
+        if (!EditorUtility.DisplayDialog(Title, $"Found {reportPaths.Count} empty folders.", "Start Clearing",
+                "Cancel")) return;
+
+        var count = 0;
+        reportPaths.ForEach(path =>
+        {
+            EditorUtility.DisplayProgressBar(Title, $"Clearing Empty Folder\n{path}", (float)count / reportPaths.Count);
+            AssetDatabase.DeleteAsset(path);
+            count++;
+        });
+        EditorUtility.ClearProgressBar();
+    }
+}
