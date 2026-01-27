72ee8ec65b911d32fab5044a953f5c8cab51b741
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-01-27 16:17:04 +0800

    feat: Integrate Discord game SDK with plugin binaries, constants, and configuration files

---
+++ Packages/io.github.ykysnk.yky-toolkit/Runtime/Discord/Constants.cs
@@ -0,0 +1,10 @@
+﻿using JetBrains.Annotations;
+
+namespace io.github.ykysnk.ykyToolkit.Discord
+{
+    [PublicAPI]
+    internal static class Constants
+    {
+        public const string DllName = "discord_game_sdk";
+    }
+}
