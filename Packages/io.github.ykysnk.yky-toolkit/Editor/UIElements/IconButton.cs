5c325b75362e3dccaca9f9e384e7c5e519d386d8
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-02-01 08:01:49 +0800

    feat: Add `IconButton` UIElement to YKY Toolkit with support for click events and extra animations

---
+++ Packages/io.github.ykysnk.yky-toolkit/Editor/UIElements/IconButton.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace io.github.ykysnk.ykyToolkit.Editor.UIElements
+{
+    public class IconButton : VisualElement
+    {
+        public IconButton()
+        {
+            AddToClassList("icon-button");
+            RegisterCallback<ClickEvent>(_ =>
+            {
+                ExtrasAnimations.ForEach(action => action());
+                AddToClassList("flash");
+                clicked?.Invoke();
+                schedule.Execute(() => RemoveFromClassList("flash")).ExecuteLater(120);
+            });
+
+            pickingMode = PickingMode.Position;
+            transform.scale = Vector3.one;
+        }
+
+        private List<Action> ExtrasAnimations { get; } = new();
+
+        [PublicAPI]
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
+        public event Action? clicked;
+
+        [PublicAPI]
+        public void AddExtraAnimation(Action action) => ExtrasAnimations.Add(action);
+
+        public new class UxmlFactory : UxmlFactory<IconButton, UxmlTraits>
+        {
+        }
+
+        public new class UxmlTraits : VisualElement.UxmlTraits
+        {
+        }
+    }
+}
