ca011d10fb72bec50bdf5e327d20b8e1de9caf9e
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-02-03 00:02:28 +0800

    feat: Add value binding and state transitions to `IconButton` with `INotifyValueChanged` implementation

--- Packages/io.github.ykysnk.yky-toolkit/Editor/UIElements/IconButton.cs
+++ Packages/io.github.ykysnk.yky-toolkit/Editor/UIElements/IconButton.cs
@@ -7,16 +7,20 @@ using UnityEngine.UIElements;
 
 namespace io.github.ykysnk.ykyToolkit.Editor.UIElements
 {
-    public class IconButton : VisualElement
+    public class IconButton : BindableElement, INotifyValueChanged<bool>
     {
+        private bool _value;
+
         public IconButton()
         {
             AddToClassList("icon-button");
             AddToClassList("localize-tooltip");
+
             RegisterCallback<ClickEvent>(_ =>
             {
                 ExtrasAnimations.ForEach(action => action());
                 AddToClassList("flash");
+                value = !value;
                 clicked?.Invoke();
                 schedule.Execute(() => RemoveFromClassList("flash")).ExecuteLater(120);
             });
@@ -27,6 +31,28 @@ namespace io.github.ykysnk.ykyToolkit.Editor.UIElements
 
         private List<Action> ExtrasAnimations { get; } = new();
 
+        public bool value
+        {
+            get => _value;
+            set
+            {
+                if (_value == value) return;
+                var previous = _value;
+                _value = value;
+
+                using var evt = ChangeEvent<bool>.GetPooled(previous, value);
+                evt.target = this;
+                SendEvent(evt);
+                UpdateVisualState();
+            }
+        }
+
+        public void SetValueWithoutNotify(bool newValue)
+        {
+            _value = newValue;
+            UpdateVisualState();
+        }
+
         [PublicAPI]
         [SuppressMessage("ReSharper", "InconsistentNaming")]
         public event Action? clicked;
@@ -34,12 +60,30 @@ namespace io.github.ykysnk.ykyToolkit.Editor.UIElements
         [PublicAPI]
         public void AddExtraAnimation(Action action) => ExtrasAnimations.Add(action);
 
+        private void UpdateVisualState()
+        {
+            EnableInClassList("on", _value);
+            EnableInClassList("off", !_value);
+        }
+
         public new class UxmlFactory : UxmlFactory<IconButton, UxmlTraits>
         {
         }
 
-        public new class UxmlTraits : VisualElement.UxmlTraits
+        public new class UxmlTraits : BindableElement.UxmlTraits
         {
+            private readonly UxmlBoolAttributeDescription _value = new()
+            {
+                name = "value"
+            };
+
+            public override void Init(VisualElement ve, IUxmlAttributes bag, CreationContext cc)
+            {
+                base.Init(ve, bag, cc);
+
+                var field = (IconButton)ve;
+                field.SetValueWithoutNotify(_value.GetValueFromBag(bag, cc));
+            }
         }
     }
 }
