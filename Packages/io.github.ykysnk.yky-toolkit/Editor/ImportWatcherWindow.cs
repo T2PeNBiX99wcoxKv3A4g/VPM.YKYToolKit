4d2ebbe69447520e47f317a489bda7d7df5b7ae6
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-02-04 07:32:49 +0800

    feat: Add file-specific highlight color management in `ImportWatcherWindow` with `ListView` binding and persistence

--- Packages/io.github.ykysnk.yky-toolkit/Editor/ImportWatcherWindow.cs
+++ Packages/io.github.ykysnk.yky-toolkit/Editor/ImportWatcherWindow.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using io.github.ykysnk.utils.Editor;
+using io.github.ykysnk.utils.NonUdon;
 using UnityEditor;
 using UnityEditor.UIElements;
 using UnityEngine;
@@ -11,6 +13,9 @@ namespace io.github.ykysnk.ykyToolkit.Editor
         private const string Title = "Import Watcher";
 
         [SerializeField] private VisualTreeAsset? uxml;
+        [SerializeField] private List<ImportWatcherFileColor> fileColors = new();
+
+        private void OnDestroy() => SaveFileColorList();
 
         private void CreateGUI()
         {
@@ -19,6 +24,7 @@ namespace io.github.ykysnk.ykyToolkit.Editor
             InternalLocalizationExtensions.Helper.UILocalize(tree);
             tree.Bind(serializedObject);
             rootVisualElement.Add(tree);
+            LoadFileColorList();
 
             var colorField = tree.Q<ColorField>("color");
             colorField.value = ColorUtility.TryParseHtmlString(EditorPrefs.GetString(ImportWatcher.ImportHighlightColor),
@@ -34,6 +40,35 @@ namespace io.github.ykysnk.ykyToolkit.Editor
                 EditorPrefs.GetFloat(ImportWatcher.ImportHighlightDuration, (float)ImportWatcher.DefaultDuration);
             doubleField.RegisterValueChangedCallback(evt =>
                 EditorPrefs.SetFloat(ImportWatcher.ImportHighlightDuration, (float)evt.newValue));
+
+            var fileColorsField = tree.Q<ListView>("fileColors");
+            var addButton = fileColorsField.Q<Button>("unity-list-view__add-button");
+            var removeButton = fileColorsField.Q<Button>("unity-list-view__remove-button");
+
+            addButton.clicked += SaveFileColorList;
+            removeButton.clicked += SaveFileColorList;
+        }
+
+        private void LoadFileColorList()
+        {
+            var json = EditorPrefs.GetString(ImportWatcher.ImportHighlightFileColor);
+
+            if (string.IsNullOrEmpty(json) ||
+                !JsonUtils.TryFromJson<ListWrapper<ImportWatcherFileColor>>(json, out var colors, out _))
+            {
+                fileColors.Clear();
+                fileColors.AddRange(ImportWatcherFileColor.DefaultColors);
+                return;
+            }
+
+            fileColors.Clear();
+            fileColors.AddRange(colors!);
+        }
+
+        private void SaveFileColorList()
+        {
+            if (!JsonUtils.TryToJson(Wrapper.Create(fileColors), out var json, out _)) return;
+            EditorPrefs.SetString(ImportWatcher.ImportHighlightFileColor, json);
         }
 
         [MenuItem("Tools/YKYToolkit/Import Watcher Window")]
