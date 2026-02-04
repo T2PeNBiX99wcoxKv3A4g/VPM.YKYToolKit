8be39d67b6681e1da638d94578c4fe2376388391
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-02-05 06:58:23 +0800

    refactor: Add `ImportSessionEditor` with custom property drawer for `ImportSession`

---
+++ Packages/io.github.ykysnk.yky-toolkit/Editor/ImportSessionEditor.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace io.github.ykysnk.ykyToolkit.Editor
+{
+    [CustomPropertyDrawer(typeof(ImportSession))]
+    public class ImportSessionEditor : PropertyDrawer
+    {
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        {
+            var root = new VisualElement();
+            root.style.marginBottom = 8;
+            root.style.marginTop = 4;
+            root.style.borderBottomWidth = 1;
+            root.style.borderBottomColor = new Color(0.2f, 0.2f, 0.2f, 0.5f);
+
+            var unixSecondsProperty = property.FindPropertyRelative("unixSeconds");
+            var recordsProperty = property.FindPropertyRelative("records");
+
+            var time = DateTimeOffset.FromUnixTimeSeconds(unixSecondsProperty.longValue).LocalDateTime
+                .ToString("yyyy-MM-dd HH:mm:ss");
+
+            var foldout = new Foldout();
+            foldout.text = $"{time} ({recordsProperty.arraySize} items)";
+            foldout.value = false;
+            foldout.style.unityFontStyleAndWeight = FontStyle.Bold;
+
+            var listView = new ListView();
+            listView.bindingPath = "records";
+            listView.showAlternatingRowBackgrounds = AlternatingRowBackground.ContentOnly;
+            listView.virtualizationMethod = CollectionVirtualizationMethod.DynamicHeight;
+            listView.selectionType = SelectionType.Multiple;
+            listView.style.marginLeft = 16;
+            listView.showBoundCollectionSize = false;
+
+            foldout.Add(listView);
+            root.Add(foldout);
+
+            return root;
+        }
+    }
+}
